7ac09248127af45e72feb2ca774080884a2f4503
Ravichandran-RP <20026389@myrp.edu.sg>
2022-05-14 11:52:26 +0800

    halfway

--- app/src/main/java/sg/edu/rp/c346/id20026389/l04_reservation/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20026389/l04_reservation/MainActivity.java
@@ -3,12 +3,17 @@ package sg.edu.rp.c346.id20026389.l04_reservation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
 
+    TimePicker tp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tp=findViewById(R.id.timePicker);
+
     }
 }
